25210c9a06a0f6144efde4ffe1fb876849daf4ae
huangche007 <hc386271623@163.com>
2020-05-09 19:10:57 +0800

    处理刷新页面，子菜单定位问题

--- components/Header.vue
+++ components/Header.vue
@@ -4,19 +4,19 @@
       <a-menu
         theme="dark"
         mode="horizontal"
-        :default-selected-keys="['1']"
+        :default-selected-keys="[mKey]"
         :style="{ lineHeight: '64px' }"
       >
-        <a-menu-item key="1">
+        <a-menu-item key="in_theaters">
           <nuxt-link to="/movie/in_theaters">
             正在热映
           </nuxt-link>
           
         </a-menu-item>
-        <a-menu-item key="2">
+        <a-menu-item key="coming_soon">
           <nuxt-link to="/movie/coming_soon">即将上映</nuxt-link>
         </a-menu-item>
-        <a-menu-item key="3">
+        <a-menu-item key="top250">
            <nuxt-link to="/movie/top250">Top250</nuxt-link>
         </a-menu-item>
       </a-menu>
@@ -25,6 +25,10 @@
 </template>
 <script>
 export default {
-    
+    computed:{
+        mKey(){
+           return this.$route.path.split('/')[2];
+        }
+    }
 }
 </script>
